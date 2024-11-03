fdf33c1cb7c8702ba1f7562f4815a8bd5b5febdb
Marcin Biłka <bilkadev@gmail.com>
2024-11-03 11:45:46 +0100

    feat:  added test to methode getRequestsDetails

--- src/app/modules/currency/services/currency.service.spec.ts
+++ src/app/modules/currency/services/currency.service.spec.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { CurrencyData, CurrencyPostResponse } from '../../core/models/currency.model';
 import { environment } from '../../../../environments/environment';
+import { RequestsDetailsResponse } from '../../core/models/requests.model';
 
 describe('CurrencyService', () => {
   let service: CurrencyService;
@@ -31,7 +32,7 @@ describe('CurrencyService', () => {
     httpTesting.verify();
   });
 
-  it('should post input numbers and update ticket', () => {
+  it('should post currency and update result', () => {
     const mockData: CurrencyData = { name: 'Test', currency: 'USD' };
     const mockResponse: CurrencyPostResponse = {
       value: '0.12345',
@@ -44,7 +45,7 @@ describe('CurrencyService', () => {
     req.flush(mockResponse);
   });
 
-  it('should give an error and show snackbar if post ticket fails', () => {
+  it('should give an error and show snackbar if post currency fails', () => {
     const mockData: CurrencyData = { name: 'Test', currency: 'USD' };
 
     service.postCurrency(mockData).subscribe({
@@ -56,6 +57,39 @@ describe('CurrencyService', () => {
     });
     const req = httpTesting.expectOne(`${environment.apiUrl}/currencies/get-current-currency-value-command`);
     expect(req.request.method).toBe('POST');
+    req.flush('Failed!', { status: 500, statusText: 'Internal Server Error' });
+  });
+
+  it('should get all request details', () => {
+    const mockResponse: RequestsDetailsResponse[] = [
+      {
+        currency: 'USD',
+        value: '0.12345',
+        name: 'Test',
+        date: '2024-01-01',
+      },
+    ];
+    service.getRequestsDetail().subscribe((res) => {
+      expect(res[0].name).toEqual('Test');
+      expect(res[0].value).toEqual('0.12345');
+      expect(res[0].date).toEqual('2024-01-01');
+      expect(res[0].currency).toEqual('USD');
+    });
+    const req = httpTesting.expectOne(`${environment.apiUrl}/currencies/requests`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+
+  it('should give an error and show snackbar if post currency fails', () => {
+    service.getRequestsDetail().subscribe({
+      next: () => fail('the post method should be failed'),
+      error: (err) => {
+        expect(err.status).toBe(500);
+        expect(snackbarServiceSpy.openSnackBar).toHaveBeenCalled();
+      },
+    });
+    const req = httpTesting.expectOne(`${environment.apiUrl}/currencies/requests`);
+    expect(req.request.method).toBe('GET');
     req.flush('Failed!', { status: 500, statusText: 'Internal Server Error' });
   });
 });
